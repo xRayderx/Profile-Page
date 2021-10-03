b23ac38b96915ed6602fb9dfb152c49169fc4468
Jose Martinez <33965736+xRayderx@users.noreply.github.com>
2021-10-03 18:21:48 -0400

    Add files via upload

---
+++ Profile Page/js/script.js
@@ -0,0 +1,9 @@
+/* Boton para abrir y cerrar el menu de navegacion lateral */
+
+function abrirNav(){
+	document.getElementById("navLateral").style.width = "100%";
+}
+
+function cerrarNav(){
+	document.getElementById("navLateral").style.width = "0";
+}
